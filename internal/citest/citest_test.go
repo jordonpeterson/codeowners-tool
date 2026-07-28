df034eb1c3d4c779359ba2ce370830e421a83e3a
jordonpeterson <jordonpropm@gmail.com>
2026-07-27 20:30:35 -0600

    test: flip CI gate test to passing to verify green path unblocks merge

--- internal/citest/citest_test.go
+++ internal/citest/citest_test.go
@@ -3,7 +3,9 @@ package citest
 import "testing"
 
 // Temporary test used to verify that the CI pipeline blocks merges when tests
-// fail. This is flipped to pass in a follow-up commit.
+// fail and permits them when tests pass.
 func TestCIGate(t *testing.T) {
-	t.Fatal("intentional failure: verifying that CI blocks merge on failing tests")
+	if 1+1 != 2 {
+		t.Fatal("math is broken")
+	}
 }
